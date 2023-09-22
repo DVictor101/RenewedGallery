c253f42f9cd4c93191dc8fe7f60f7b9175322f40
Segun(RenewedCrown) <seguntimileyin489@gmail.com>
2023-09-22 14:28:28 +0100

    resolved hardcoding signup data

--- src/Pages/SignInPage/SignIn.js
+++ src/Pages/SignInPage/SignIn.js
@@ -1,8 +1,9 @@
-import "./signin.scss";
-import { useState } from "react";
+
+import "./signin.scss"
+import React, { useState } from "react";
 import {
- getAuth,
- signInWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import app from "../../Firebase";
@@ -10,85 +11,59 @@ import app from "../../Firebase";
 const auth = getAuth(app);
 
 const SignIn = () => {
- const [email, setEmail] = useState("");
- const [password, setPassword] = useState("");
- const [error, setError] = useState(null);
- const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
- const handleSignIn = async (e) => {
-  e.preventDefault();
+  const handleSignIn = async (e) => {
+    e.preventDefault();
 
-  try {
-   if (
-    email === "user@example.com" &&
-    password === "1Password"
-   ) {
-    await signInWithEmailAndPassword(
-     auth,
-     email,
-     password
-    );
-    setError(null);
-    navigate("/homepage");
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setError(null);
+      navigate("/homepage");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.error("Error signing in:", error);
+      setError("Error signing in, check your email and password.");
+    }
+  };
 
-    setEmail("");
-    setPassword("");
-   } else {
-    setError(
-     `Access not granted. Only use permitted login details.`
-    );
-   }
-  } catch (error) {
-   console.error("Error signing in:", error);
-   setError(
-    "Error Signing in, check connection."
-   );
-
-   setEmail("");
-   setPassword("");
-  }
- };
-
- return (
-  <div className="signin_cont">
-   <div className="signin">
-    <form
-     className="signinform"
-     onSubmit={handleSignIn}
-    >
-     <label className="signin_text">Email</label>
-     <input
-      type="email"
-      placeholder="myemail@gmail.com"
-      className="input"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-     />
-     <label className="signin_text">
-      Password
-     </label>
-     <input
-      type="password"
-      placeholder="Enter Password"
-      className="input"
-      value={password}
-      onChange={(e) =>
-       setPassword(e.target.value)
-      }
-     />
-     <button
-      type="submit"
-      className="custom-button modi-btn"
-     >
-      LOG In
-     </button>
-     {error && (
-      <p className="error-text">{error}</p>
-     )}
-    </form>
-   </div>
-  </div>
- );
+  return (
+    <div className="signin_cont">
+      <div className="signin">
+        <form className="signinform" onSubmit={handleSignIn}>
+          <label className="signin_text">Email</label>
+          <input
+            type="email"
+            placeholder="myemail@gmail.com"
+            className="input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label className="signin_text">Password</label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="custom-button modi-btn"
+          >
+            Log In
+          </button>
+          {error && (
+            <p className="error-text">{error}</p>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
